d89c0a30f121e469a1725b8085371ecc671cad7e
huvitutti <donhovila@proton.me>
2024-12-23 12:12:48 +0200

    Gitignore tehty ja tyhjä alusta

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pygame
+from constants import *
 
-print("Starting asteroids!")
+print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+running = True
+while running:
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
